f7623e985bdcc862e1074d70675f20b627ec111b
Kurichenko Oleksii <ok@reclamare.ua>
2016-05-06 16:42:44 +0300

    works with params now

--- addon/components/input-mask.js
+++ addon/components/input-mask.js
@@ -5,14 +5,30 @@ import Ember from 'ember';
 export default Ember.TextField.extend({
   initializeMask: function() {
     var mask = this.get('mask');
+    let params = {};
+    let self = this;
 
-    this.$().inputmask(mask, {
-      onBeforeMask: function(value) {
-        if (mask === 'mm/dd/yyyy') {
-          return moment(new Date(value)).format('L');
+    let passedParams = this.attrs;
+
+    for (let i in passedParams) {
+      if(typeof passedParams[i] !== 'undefined' && passedParams[i] !== null) {
+        if(typeof Inputmask.prototype.defaults[i] === 'function'){
+          params[i] = function() {
+            self.sendAction(i,...arguments);
+          }
+        }else {
+          params[i] = passedParams[i];
         }
       }
-    });
+    }
+
+    params.onBeforeMask = function(value) {
+      if (mask === 'mm/dd/yyyy') {
+        return moment(new Date(value)).format('L');
+      }
+    };
+
+    this.$().inputmask(mask, params);
 
     // The input mask changes the value of the input from the original to a
     // formatted version. We need to manually send that change back to the
